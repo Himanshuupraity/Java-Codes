0000f192ea621f36397798dd0bffbc0e7c999ba3
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2025-05-20 09:39:29 +0530

    Variables
    A variable in Java is a container (like a box) that stores data values during the execution of a program.

---
+++ variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String[] args) {
+        String name= "Tony stark";
+        int age = 48;
+        double price = 25.5;
+        int a = 25;
+        int b=20;
+        
+    }
+}
