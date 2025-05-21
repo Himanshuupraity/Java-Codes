14188294ada307f839cf45110ec45c84774382c5
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2025-05-21 09:48:55 +0530

    print and println
    There is the difference in print and println
    - print is used when we need to print the output in one line
    - prinln is used when we need to print output in next line

---
+++ print&println.java
@@ -0,0 +1,10 @@
+class firstclass{
+    public static void main(String[] args) {
+        // println
+        System.out.println("hello World");
+
+        // print using \n
+        System.out.print("Hello this is me \n");
+        System.out.print("Hello this is me again");
+    }
+}
