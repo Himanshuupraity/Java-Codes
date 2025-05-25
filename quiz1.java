84f580ed39d19642943ed5f36537018d6b83d6af
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2025-05-25 12:09:34 +0530

    quiz

---
+++ quiz1.java
@@ -0,0 +1,15 @@
+// calculte a*b/a-b
+
+public class quiz1 {
+    public static void main(String[] args) {
+        int a=10;
+        int b = 5;
+
+        int mul = a*b;
+        int sub = a-b;
+
+        int calculate = mul/sub;
+
+        System.out.println(calculate);
+    }
+}
