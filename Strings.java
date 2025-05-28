e8b18169d70c7bc61cfabbb7ab51ff46192bcdf6
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2025-05-28 09:45:07 +0530

    strings

---
+++ Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        // String declaration
+        String name = "Tonny";
+        String fullName = "Tony Stark";
+        String sentence = "My name is Tony Stark";
+
+        System.out.println(name);
+        System.out.println(fullName);
+        System.out.println(sentence);
+    }
+}
