8301a8c0bcc8b190749e990b85fd3f51f43af318
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2025-05-31 09:51:34 +0530

    Array
    Array basic code

---
+++ array.java
@@ -0,0 +1,14 @@
+public class array {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 97;
+        marks[1] = 48;
+        marks[2] = 56;
+
+        for(int i=0; i<3; i++){
+            // System.out.println(marks[i]);
+            System.out.println(marks[4]);
+        }
+    }
+    
+}
