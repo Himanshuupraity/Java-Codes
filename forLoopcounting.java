ac8f5cde291410bf108b10d45dc5690c8f81c106
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2025-06-10 12:57:45 +0530

    counting using for loop

---
+++ forLoopcounting.java
@@ -0,0 +1,11 @@
+public class forLoopcounting {
+    public static void main(String[] args) {
+        // for(int counter = 1; counter < 11; counter++){
+        //     System.out.print(counter+" ");
+        // }
+
+        for(int i=1; i<11; i++){
+            System.out.println(i);
+        }
+    }
+}
