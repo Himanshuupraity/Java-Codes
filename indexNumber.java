741bac182edfb34e212e2be68552e50f1b45ede2
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2025-06-24 09:53:03 +0530

    Index number
    Index number function

---
+++ indexNumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class indexNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = str.charAt(3);
+        System.out.println(c);
+    }
+
+}
