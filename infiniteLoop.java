706d78dc225f0b1f4c81df244c1595a592c39ff8
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2025-06-25 11:46:39 +0530

    infinite loop
    infinite loop code

---
+++ infiniteLoop.java
@@ -0,0 +1,7 @@
+public class infiniteLoop {
+    public static void main(String[] args) {
+        do { 
+            System.out.println("This will print forever");
+        } while (true);
+    }
+}
