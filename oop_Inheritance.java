e9cab970d6f87836610769bdbba364aa5dd411e6
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2025-07-01 11:24:23 +0530

    opps inheritance
    Inheritance java

---
+++ oop_Inheritance.java
@@ -0,0 +1,14 @@
+class shape{
+    String color;
+}
+
+class Triangle extends shape{
+
+}
+
+public class oop_Inheritance {
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle();
+        t1.color = "red";
+    }
+}
